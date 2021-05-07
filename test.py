538644d21a20f8220df8ba94bb6802cd6548db11
rinszk <rinszk@gmail.com>
2021-05-07 14:26:48 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 import requests
 
-
-r = requests.get("https://www.google.com/")
-print(f'status code: {r.status_code}')
-
+while True:
+    try: 
+        uri = input()
+        r = requests.get(uri)
+        print(f'uri: {uri}')
+        print(f'status code: {r.status_code}')
+        print(f'content length: {len(r.content)} bytes')
+        print(f'elapsed time: {r.elapsed.total_seconds()} s')
+    except EOFError:
+        break    
